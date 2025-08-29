b39f447d5f4cbfd6066279af05ab77221546df9a
bozinovski.slobodan@gmail.com <bozinovski.slobodan@gmail.com>
2025-08-29 12:58:55 +0000

    daily weather

--- models/data_dimension.sql
+++ models/data_dimension.sql
@@ -4,7 +4,9 @@ with cte as (
     DATE(TO_TIMESTAMP(STARTED_AT)) as date_started_at,
     HOUR(TO_TIMESTAMP(STARTED_AT)) as hour_started_at,
     MONTH(TO_TIMESTAMP(STARTED_AT)) as month_started_at,
-    CASE WHEN DAYNAME(TO_TIMESTAMP(STARTED_AT)) in ('Sat','Sun') then 'WEKKEND' ELSE 'BUSINESDAY' END as day_type,
+    {{day_type('STARTED_AT')}} as day_type,
+   {{get_season('STARTED_AT')}} as season
+
     from {{ source('demo', 'bike') }}
     where STARTED_AT != 'started_at'
 )
